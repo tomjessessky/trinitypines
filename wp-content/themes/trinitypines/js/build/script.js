42a980345357286036958c742b541a4b0b4f0be3
Kris Dockter <kriskd@gmail.com>
2013-12-07 16:47:24 -0600

    Work on the accordian

--- wp-content/themes/trinitypines/js/build/script.js
+++ wp-content/themes/trinitypines/js/build/script.js
@@ -511,5 +511,12 @@
         $(this).tab('show')
     });
     
-    $('#accordion').accordion();
+    $("#accordion").accordion({
+        activate: function(event, ui) {
+            ui.newHeader.find('span').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            ui.oldHeader.find('span').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        }
+    });
+    
+    $("#accordion").accordion("option", "icons", null);
 });
--- wp-content/themes/trinitypines/js/src/script.js
+++ wp-content/themes/trinitypines/js/src/script.js
@@ -5,5 +5,12 @@ $(document).ready(function(){
         $(this).tab('show')
     });
     
-    $('#accordion').accordion();
+    $("#accordion").accordion({
+        activate: function(event, ui) {
+            ui.newHeader.find('span').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            ui.oldHeader.find('span').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        }
+    });
+    
+    $("#accordion").accordion("option", "icons", null);
 });
