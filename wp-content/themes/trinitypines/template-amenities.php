808589675d0cebf3934e5afc335420b7f69e58f6
Kris Dockter <kriskd@gmail.com>
2013-12-07 14:03:04 -0600

    Start of amenities page

--- wp-content/themes/trinitypines/template-amenities.php
+++ wp-content/themes/trinitypines/template-amenities.php
@@ -5,8 +5,94 @@ Template Name: Amenities
 
 require("header.php");
 ?>
-<h1>Amenities</h1>
 
+<div class="row">
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+        <h2>Amenities</h2>
+        <p>Life is easy at Trinity Pines. We offer:</p>
+        <ul>
+            <li>Heated Underground Parking</li>
+            <li>Use of Dining Room for Family Events</li>
+            <li>Elevator to all Floors - no stairs to climb</li>
+            <li>Cable TV (expanded channels)</li>
+            <li>Indoor Mailboxes</li>
+            <li>Hot Water Heat</li>
+            <li>Paid Water/Sewer</li>
+            <li>Extra Storage Space</li>
+            <li>Library</li>
+            <li>Whirlpool Tub</li>
+            <li>Beauty & Barber Shop Room</li>
+            <li>Excercise Room & Billiards Table</li>
+            <li>Gardening Areas</li>
+            <li>Craft Room</li>
+            <li>Indoor Waste & Recycling</li>
+            <li>24-hour Secured Entrance</li>
+            <li>Full Sprinkler & Fire Detection Systems</li>
+            <li>Maintenance Service</li>
+            <li>... all at no extra charge!</li>
+        </ul>
+        <h2>Dining Program</h2>
+        <p>Our experienced cooks pride themselves in creating a nutritionally
+        balanced and delicious noon meal each day... always served on tables dressed with linen.</p>
+ 
+        <p>No tipping necessary!
+        Meals are served at noon Monday through Friday, cooked fresh on-site and served by waitstaff
+        on china.   Many residents eat everyday, but residents are free to select from the advanced
+        menu published weekly and are asked to eat a minimum of four times per month.</p>
+    </div>
+    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
+        <img src="http://placehold.it/350x150">
+        <p>Fee Schedule:</p>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                Apartment Style
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                Entry Fee
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                Monthly Rent
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                One Bedroom
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                $35,000 - $40,000
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                $536 - $680
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                Two Bedroom
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                $45,000 - $55,000
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                $742 - $889
+            </div>
+        </div>
+        <p>
+            You will be happy to discover that we have a lower-than-market Entry Fee and a
+            higher-than-market refund.
+        </p>
+ 
+        <p>
+            Upon your departure from Trinity Pines, a minimum of 85% of your Entry Fee will
+            be refunded to you or your estate after your apartment has been re-rented.
+        </p>
+ 
+        <p>
+            Monthly rent includes draperies, heat, water/sewer, expanded cable TV, underground
+            parking, garbage pick-up, air conditioner, extra storage, use of whirlpool spa,
+            exercise room, billiards room, craft room and on-call maintenance.
+        </p>
+    </div>
+</div>
 
 <?php
 require("footer.php");
