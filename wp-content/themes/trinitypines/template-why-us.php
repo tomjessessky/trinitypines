281a2acadb12e2e8d6238a971fb09b77dd492147
Tom Jessessky <tomjessessky@gmail.com>
2013-12-07 18:30:25 -0600

    why us

--- wp-content/themes/trinitypines/template-why-us.php
+++ wp-content/themes/trinitypines/template-why-us.php
@@ -7,14 +7,26 @@ require("header.php");
 ?>
 
 <div class="why-us-wrapper">
+    <div class="why-us-header-wrapper">
+        <div class="why-us-header container">
+            <div class="row">
+                <div class="col-lg-5 col-lg-offset-1 col-md-6 col-sm-6 col-xs-6">
+                    <img src="/wp-content/images/best-friends.jpg" class="img-bordered why-us-best-friends" />
 
-        <div class="why-us-header">
-            <p>Why Choose Trinity?</p>
+                </div>
+                <div class="col-lg-3 col-lg-offset-1 col-md-5 col-sm-5 col-xs-5" style="padding-top: 40px;">
+                    <h2>WHY</h2>
+                    <h2>CHOOSE</h2>
+                    <h2>TRINITY?</h2>
+                </div>
+            </div>
         </div>
+    </div>
         <div class="accordian-wrapper">
             <div id="accordion">
                 <h3>Independence<span class="glyphicon glyphicon-chevron-up"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     Independence is easy!  There is so much to see and do in Lake Mills from
                     playing a round of golf, to cruising or fishing on Rock Lake, weekly Farmer's
@@ -22,9 +34,14 @@ require("header.php");
                     with the peace of mind knowing your apartment is secure.  Are you a snowbird?
                     We'll take care of things while you are away!  
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>Friendship<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     Neighbors become friends, and old friends are now neighbors! There is plenty
                     of opportunity to socialize here at Trinity Pines.  Meet a group down in the
@@ -33,9 +50,14 @@ require("header.php");
                     baby grand piano. Perhaps you'd like to sit and chat by the fireplace with
                     a cup of coffee?    
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>Convenience<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     Each apartment home has an electric range,  refrigerator/freezer, garbage
                     disposal, stainless steel double sink, draperies, wax-free vinyl flooring,
@@ -52,9 +74,14 @@ require("header.php");
                     with the area medical community; including home health services, priority and
                     placement contracts with local assisted living and nursing home facilities.
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>Security<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     The concerns for our own personal safety is a fear many of us have as we get
                     older. For your peace of mind, Trinity Pines offers a morning check-in light
@@ -65,9 +92,14 @@ require("header.php");
                     <p>
                     Interior mailboxes mean a safer walk to get your mail - no matter the weather!
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>FulFillment<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     Trinity Pines has recreational areas for the playfulness we never outgrow and
                     workshops for the hobbies and crafts we enjoy. Gardeners will enjoy both sunny
@@ -76,18 +108,28 @@ require("header.php");
                     a game of cards in the library. Some attend Bible study or the Ecumenical
                     Service, although Trinity Pines is nondenominational.
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>Comfort &amp; Beauty<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     When you drive into Trinity Pines, the first thing you will notice is the
                     impressive architecture and beautifully landscaped grounds. Inside you will
                     feel yourself surrounded by the quality and comfort of our fireside lounge,
                     library, excercise room, hair salon, and fine dining room.
                     </p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
                 <h3>Social Activites<span class="glyphicon glyphicon-chevron-down"></span></h3>
-                <div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
                     <p>
                     So much to do.  Trinity Pines organizes a wide range of activities for
                     residents to participate in if they so choose.  Residents are free to join
@@ -98,6 +140,9 @@ require("header.php");
                     screening, pedicures, coffee klatsches, card games, and even special excursions
                     to the theater.  
                     </p>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <img height="150px" src="/wp-content/images/best-friends.jpg" />
+                    </div>
                 </div>
             </div>
         </div>
