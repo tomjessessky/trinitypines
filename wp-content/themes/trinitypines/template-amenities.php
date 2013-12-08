72c478289c32b2ce5a93cb41d746860d1cab97b1
Kris Dockter <kriskd@gmail.com>
2013-12-07 18:29:14 -0600

    Amentities

--- wp-content/themes/trinitypines/template-amenities.php
+++ wp-content/themes/trinitypines/template-amenities.php
@@ -7,9 +7,9 @@ require("header.php");
 ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-            <h2>Amenities</h2>
+    <h2>Amenities &amp; Services</h2>
+    <div class="row life-easy">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
             <p>Life is easy at Trinity Pines. We offer:</p>
             <ul>
                 <li>Heated Underground Parking</li>
@@ -20,6 +20,10 @@ require("header.php");
                 <li>Hot Water Heat</li>
                 <li>Paid Water/Sewer</li>
                 <li>Extra Storage Space</li>
+            </ul>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            <ul>
                 <li>Library</li>
                 <li>Whirlpool Tub</li>
                 <li>Beauty & Barber Shop Room</li>
@@ -32,6 +36,67 @@ require("header.php");
                 <li>Maintenance Service</li>
                 <li>... all at no extra charge!</li>
             </ul>
+        </div>
+    </div>
+    <div class="row fee-schedule">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <p>Fee Schedule</p>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            <p>You will be happy to discover that we have a lower-than-market Entry Fee and a
+            higher-than-market refund.</p>
+
+            <p>Upon your departure from Trinity Pines, a minimum of 85% of your Entry Fee will
+            be refunded to you or your estate after your apartment has been re-rented.</p>
+
+            <p>Monthly rent includes draperies, heat, water/sewer, expanded cable TV, underground
+            parking, garbage pick-up, air conditioner, extra storage, use of whirlpool spa,
+            exercise room, billiards room, craft room and on-call maintenance.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <p></p>If you have been checking all of the links on our home page, you might be
+            saying, "With all that is included, it must be expensive!"   The fact is that when
+            you add up the property taxes and all of the expenses that go into maintaining the
+            typical house, living at Trinity Pines is very economical.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            Apartment Style
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            Entry Fee
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            Monthly Rent
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            One Bedroom
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            $35,000 - $40,000
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            $536 - $680
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            Two Bedroom
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            $45,000 - $55,000
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            $742 - $889
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
             <h2>Dining Program</h2>
             <p>Our experienced cooks pride themselves in creating a nutritionally
             balanced and delicious noon meal each day... always served on tables dressed with linen.</p>
@@ -41,42 +106,8 @@ require("header.php");
             on china.   Many residents eat everyday, but residents are free to select from the advanced
             menu published weekly and are asked to eat a minimum of four times per month.</p>
         </div>
-        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 amentities-top-right">
-            <img src="http://placehold.it/600x400">
-            <p>Fee Schedule:</p>
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    Apartment Style
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    Entry Fee
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    Monthly Rent
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    One Bedroom
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    $35,000 - $40,000
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    $536 - $680
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    Two Bedroom
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    $45,000 - $55,000
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    $742 - $889
-                </div>
-            </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            
             <p>
                 You will be happy to discover that we have a lower-than-market Entry Fee and a
                 higher-than-market refund.
@@ -94,53 +125,47 @@ require("header.php");
             </p>
         </div>
     </div>
-</div>
-<div class="amenities-floorplan-wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                <ul class="nav nav-tabs">
-                    <li class="active"><a href="#one">One Bed</a></li>
-                    <li><a href="#two">Two Bed</a></li>
-                </ul>
-                <div class="tab-content">
-                    <div class="tab-pane active" id="one">
-                        One Bedroom Layout
-                    </div>
-                    <div class="tab-pane" id="two">
-                        Two Bedroom Layout
-                    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <p>All Apartments are Appointed With:</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            <p>Full kitchen with self-cleaning oven, stove, refrigerator, garbage disposal, lazy susan,
+            and under-counter light</p>
+            <p>Full bathroom with high toilet and low threshold shower</p>
+            <p>Large, lighted, closets </p>
+            <p>Air conditioner</p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            <p>Cable TV (expanded channels)</p>
+            <p>Morning “check-in” button</p>
+            <p>Careline ready phone jack</p>
+            <p>Sprinkler System and smoke alarms</p>
+            <p>Storage room (in addition to basement storage)</p>
+            <p>Oak Woodwork</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#one">One Bed</a></li>
+                <li><a href="#two">Two Bed</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="one">
+                    One Bedroom Layout
+                </div>
+                <div class="tab-pane" id="two">
+                    Two Bedroom Layout
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                <p>We have several different floorplans at Trinity Pines.</p>
-
-                <p>Spacious floorplan options range from 630 square feet (one bedroom) to 988 square feet (two bedrooms).</p>
-                 
-                <p>Some apartment homes have patios on the first floor or balconies on the second floor.</p>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                <p>We have several different floorplans at Trinity Pines.</p>
-                 
-                <p>Spacious floorplan options range from 630 square feet (one bedroom) to 988 square feet (two bedrooms).</p>
-                 
-                <p>Some apartment homes have patios on the first floor or balconies on the second floor.</p>
-                 
-                <p>All apartments are appointed with:</p>
-                <ul>
-                    <li>Full kitchen with self-cleaning oven, stove, refrigerator, garbage disposal,
-                    lazy susan, and under-counter light</li>
-                    <li>Full bathroom with high toilet and low threshold shower</li>
-                    <li>Large, lighted, closets</li>
-                    <li>Air conditioner</li>
-                    <li>Cable TV (expanded channels)</li>
-                    <li>Morning "check-in" button</li>
-                    <li>Careline ready phone jack</li>
-                    <li>Sprinkler System and smoke alarms</li>
-                    <li>Storage room (in addition to basement storage)</li>
-                    <li>Oak Woodwork</li>
-                </ul>
-            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+            <p>We have several different floorplans at Trinity Pines.</p>
+            <p>Spacious floorplan options range from 630 square feet (one bedroom) to 988 square feet (two bedrooms).</p>
+            <p>Some apartment homes have patios on the first floor or balconies on the second floor.</p>
         </div>
     </div>
 </div>
